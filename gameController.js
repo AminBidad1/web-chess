95dac84775212ac20bc8e646ac6ad593b3bdde19
Amin Bidad <amin.200.bidad@gmail.com>
2023-11-20 15:32:27 +0330

    Add kish rules

--- gameController.js
+++ gameController.js
@@ -4,6 +4,19 @@ let curPlayer;
 let curHeldPiece;
 let curHeldPieceStartingPosition;
 
+let piecesScores = {
+    "P": 1,
+    "N": 3,
+    "B": 3,
+    "R": 5,
+    "Q": 9,
+    "p": 1,
+    "n": 3,
+    "b": 3,
+    "r": 5,
+    "q": 9
+}
+
 function startGame() {
     const starterPosition = [['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
     ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
@@ -135,51 +148,58 @@ function setPieceHoldEvents() {
 function movePiece(piece, startingPosition, endingPosition) {
     // move validations to validateMovement()
     const boardPiece = curBoard[startingPosition[0]][startingPosition[1]];
-    
+    let alreadyPiece;
     if (boardPiece != '.') {
         if ((boardPiece === boardPiece.toUpperCase() && curPlayer == 'black') ||
             (boardPiece === boardPiece.toLowerCase() && curPlayer == 'white')) {
+                alreadyPiece = curBoard[endingPosition[0]][endingPosition[1]];
                 curBoard[startingPosition[0]][startingPosition[1]] = '.';
+                score(endingPosition);
+                endGame(endingPosition);
                 curBoard[endingPosition[0]][endingPosition[1]] = boardPiece;
-
-                const destinationSquare = document.getElementById(`${endingPosition[0] + 1}${endingPosition[1] + 1}`);
-                destinationSquare.textContent = '';
-                destinationSquare.appendChild(piece);
-
-                // check if is check/checkmate
-
-                if (curPlayer == 'white') {
-                    curPlayer = 'black';
-                } else {
-                    curPlayer = 'white';
+                if (isKish()){
+                    curBoard[endingPosition[0]][endingPosition[1]] = alreadyPiece;
+                    curBoard[startingPosition[0]][startingPosition[1]] = boardPiece;
+                }
+                else {
+                    const destinationSquare = document.getElementById(`${endingPosition[0] + 1}${endingPosition[1] + 1}`);
+                    destinationSquare.textContent = '';
+                    destinationSquare.appendChild(piece);
+                    
+                    // check if is check/checkmate
+                    if (curPlayer == 'white') {
+                        curPlayer = 'black';
+                    } else {
+                        curPlayer = 'white';
+                    }
                 }
         }
     }
 }
 
-function validateMovement(startingPosition, endingPosition) {
+function validateMovement(startingPosition, endingPosition, kishCheck = false) {
     const boardPiece = curBoard[startingPosition[0]][startingPosition[1]];
     
     switch (boardPiece) {
         case 'r':
-        case 'R': return validateRookMovement(startingPosition, endingPosition);
+        case 'R': return validateRookMovement(startingPosition, endingPosition, kishCheck);
         case 'n':
-        case 'N': return validateKnightMovement(startingPosition, endingPosition);
+        case 'N': return validateKnightMovement(startingPosition, endingPosition, kishCheck);
         case 'b':
-        case 'B': return validateBishopMovement(startingPosition, endingPosition);
+        case 'B': return validateBishopMovement(startingPosition, endingPosition, kishCheck);
         case 'q':
-        case 'Q': return validateQueenMovement(startingPosition, endingPosition);
+        case 'Q': return validateQueenMovement(startingPosition, endingPosition, kishCheck);
         case 'k': 
-        case 'K': return validateKingMovement(startingPosition, endingPosition);
-        case 'p': return validatePawnMovement('white', startingPosition, endingPosition);
-        case 'P': return validatePawnMovement('black', startingPosition, endingPosition);
+        case 'K': return validateKingMovement(startingPosition, endingPosition, kishCheck);
+        case 'p': return validatePawnMovement('white', startingPosition, endingPosition, kishCheck);
+        case 'P': return validatePawnMovement('black', startingPosition, endingPosition, kishCheck);
     }
 }
 
-function validateBishopMovement(startingPosition, endingPosition) {
+function validateBishopMovement(startingPosition, endingPosition, kishCheck = false) {
     if (endingPosition[0] - endingPosition[1] == startingPosition[0] - startingPosition[1] ||
         endingPosition[0] + endingPosition[1] == startingPosition[0] + startingPosition[1]) {
-            if (!validatePathIsBlocked(startingPosition, endingPosition)) {
+            if (!validatePathIsBlocked(startingPosition, endingPosition, kishCheck)) {
                 return false;
             }
             // validate if move puts own king in check
@@ -189,9 +209,9 @@ function validateBishopMovement(startingPosition, endingPosition) {
     }
 }
 
-function validateRookMovement(startingPosition, endingPosition) {
+function validateRookMovement(startingPosition, endingPosition, kishCheck = false) {
     if (endingPosition[0] == startingPosition[0] || endingPosition[1] == startingPosition[1]) {
-        if (!validatePathIsBlocked(startingPosition, endingPosition)) {
+        if (!validatePathIsBlocked(startingPosition, endingPosition, kishCheck)) {
             return false;
         }
         // validate if move puts own king in check
@@ -201,9 +221,9 @@ function validateRookMovement(startingPosition, endingPosition) {
     }
 }
 
-function validateKingMovement(startingPosition, endingPosition) {
+function validateKingMovement(startingPosition, endingPosition, kishCheck = false) {
     if ([-1, 0, 1].includes(endingPosition[0] - startingPosition[0]) && [-1, 0, 1].includes(endingPosition[1] - startingPosition[1])) {
-        if (isFriendlyPieceOnEndingPosition(endingPosition)) {
+        if (isFriendlyPieceOnEndingPosition(endingPosition, kishCheck)) {
             return false;
         }
         // validate if move puts own king in check
@@ -214,11 +234,11 @@ function validateKingMovement(startingPosition, endingPosition) {
     }
 }
 
-function validateQueenMovement(startingPosition, endingPosition) {
+function validateQueenMovement(startingPosition, endingPosition, kishCheck = false) {
     if (endingPosition[0] - endingPosition[1] == startingPosition[0] - startingPosition[1] ||
         endingPosition[0] + endingPosition[1] == startingPosition[0] + startingPosition[1] ||
         endingPosition[0] == startingPosition[0] || endingPosition[1] == startingPosition[1]) {
-            if (!validatePathIsBlocked(startingPosition, endingPosition)) {
+            if (!validatePathIsBlocked(startingPosition, endingPosition, kishCheck)) {
                 return false;
             }
             // validate if move puts own king in check
@@ -228,7 +248,7 @@ function validateQueenMovement(startingPosition, endingPosition) {
     }
 }
 
-function validatePawnMovement(pawnColor, startingPosition, endingPosition) {
+function validatePawnMovement(pawnColor, startingPosition, endingPosition, kishCheck = false) {
     direction = pawnColor == 'black' ? 1 : -1;
 
     let isCapture = false;
@@ -236,7 +256,7 @@ function validatePawnMovement(pawnColor, startingPosition, endingPosition) {
     if (endingPosition[0] == startingPosition[0] + direction &&
         [startingPosition[1] - 1, startingPosition[1] + 1].includes(endingPosition[1])) {
             // validate if is en passant
-            if (isEnemyPieceOnEndingPosition(endingPosition)) {
+            if (isEnemyPieceOnEndingPosition(endingPosition, kishCheck)) {
                 isCapture = true;
             }
         }
@@ -250,9 +270,9 @@ function validatePawnMovement(pawnColor, startingPosition, endingPosition) {
 
     if (((endingPosition[0] == startingPosition[0] + direction || (endingPosition[0] == startingPosition[0] + direction * 2 && isFirstMove)) &&
          endingPosition[1] == startingPosition[1]) || isCapture) {
-            if (isFriendlyPieceOnEndingPosition(endingPosition)) {
+            if (isFriendlyPieceOnEndingPosition(endingPosition, kishCheck)) {
                 return false;
-            } else if (!isCapture && isEnemyPieceOnEndingPosition(endingPosition)) {
+            } else if (!isCapture && isEnemyPieceOnEndingPosition(endingPosition, kishCheck)) {
                 return false;
             }
 
@@ -263,10 +283,10 @@ function validatePawnMovement(pawnColor, startingPosition, endingPosition) {
     }
 }
 
-function validateKnightMovement(startingPosition, endingPosition) {
+function validateKnightMovement(startingPosition, endingPosition, kishCheck = false) {
     if (([-2, 2].includes(endingPosition[0] - startingPosition[0]) && [-1, 1].includes(endingPosition[1] - startingPosition[1])) || 
         ([-2, 2].includes(endingPosition[1] - startingPosition[1]) && [-1, 1].includes(endingPosition[0] - startingPosition[0]))) {
-            if (isFriendlyPieceOnEndingPosition(endingPosition)) {
+            if (isFriendlyPieceOnEndingPosition(endingPosition, kishCheck)) {
                 return false;
             }
             // validate if move puts own king in check
@@ -276,7 +296,7 @@ function validateKnightMovement(startingPosition, endingPosition) {
     }
 }
 
-function validatePathIsBlocked(startingPosition, endingPosition) {
+function validatePathIsBlocked(startingPosition, endingPosition, kishCheck = false) {
     const xDifference = endingPosition[0] - startingPosition[0]
     const yDifference = endingPosition[1] - startingPosition[1]
 
@@ -309,7 +329,7 @@ function validatePathIsBlocked(startingPosition, endingPosition) {
         squareY += yDirection;
     }
     
-    if (isFriendlyPieceOnEndingPosition(endingPosition)) {
+    if (isFriendlyPieceOnEndingPosition(endingPosition, kishCheck)) {
         return false;
     } else {
         // enemy piece has been captured
@@ -318,39 +338,138 @@ function validatePathIsBlocked(startingPosition, endingPosition) {
     return true;
 }
 
-function isFriendlyPieceOnEndingPosition(endingPosition) {
+function isFriendlyPieceOnEndingPosition(endingPosition, kishCheck = false) {
     const destinationSquare = document.getElementById(`${endingPosition[0] + 1}${endingPosition[1] + 1}`);
-
     if (destinationSquare.children.length > 0) {
         const destinationPiece = destinationSquare.querySelector('.piece').id;
-    
-        if (destinationPiece == destinationPiece.toUpperCase() && curPlayer == 'black' ||
-            destinationPiece == destinationPiece.toLowerCase() && curPlayer == 'white') {
+        const boardPiece = curBoard[endingPosition[0]][endingPosition[1]]
+        if (!kishCheck){
+            if (boardPiece == boardPiece.toUpperCase() && curPlayer == 'black' ||
+                boardPiece == boardPiece.toLowerCase() && curPlayer == 'white') {
+                    console.log(destinationPiece, curPlayer);
+                    return true;
+            } else {
+                return false;
+            }
+        }
+        else {
+            if (boardPiece == boardPiece.toUpperCase() && curPlayer == 'black' ||
+                boardPiece == boardPiece.toLowerCase() && curPlayer == 'white') {
+                    console.log("yes");
+                    return false;
+            } else {
                 return true;
-        } else {
-            return false;
-        }        
+            }
+        }
     } else {
         return false;
     }
 }
 
-function isEnemyPieceOnEndingPosition(endingPosition) {
+function isEnemyPieceOnEndingPosition(endingPosition, kishCheck = false) {
     const destinationSquare = document.getElementById(`${endingPosition[0] + 1}${endingPosition[1] + 1}`);
 
     if (destinationSquare.children.length > 0) {
         const destinationPiece = destinationSquare.querySelector('.piece').id;
-    
-        if (destinationPiece == destinationPiece.toUpperCase() && curPlayer == 'white' ||
-            destinationPiece == destinationPiece.toLowerCase() && curPlayer == 'black') {
+        const boardPiece = curBoard[endingPosition[0]][endingPosition[1]]
+        if (!kishCheck){
+            if (destinationPiece == destinationPiece.toUpperCase() && curPlayer == 'white' ||
+                destinationPiece == destinationPiece.toLowerCase() && curPlayer == 'black') {
+                    return true;
+            } else {
+                return false;
+            }
+        }
+        else {
+            if (destinationPiece == destinationPiece.toUpperCase() && curPlayer == 'white' ||
+                destinationPiece == destinationPiece.toLowerCase() && curPlayer == 'black') {
+                    return false;
+            } else {
                 return true;
-        } else {
-            return false;
-        }        
+            }
+        }
     } else {
         return false;
     }
 }
+
+
+function score(endingPosition){
+    let div_score;
+    if (curPlayer == "white") {
+        div_score = document.getElementById("white_score");
+    }
+    else {
+        div_score = document.getElementById("black_score");
+    }
+    if (curBoard[endingPosition[0]][endingPosition[1]] != '.') {
+        let enemyPiece = curBoard[endingPosition[0]][endingPosition[1]];
+        if (enemyPiece in piecesScores){
+            div_score.innerText = Number(div_score.innerText) + piecesScores[enemyPiece];
+        }
+    }
+}
+
+
+function endGame(endingPosition){
+    if (curBoard[endingPosition[0]][endingPosition[1]] == 'k'){
+        alert("Black Won!");
+    }
+    else if (curBoard[endingPosition[0]][endingPosition[1]] == 'K'){
+        alert("White Won!");
+    }
+}
+
+
+function isKish(){
+    let king_position;
+    let boardPiece;
+    if (curPlayer == "white"){
+        for (let i=0; i<8; i+=1){
+            for (let j=0; j<8; j+=1){
+                if (curBoard[i][j] == 'k'){
+                    king_position = [i, j];
+                }
+            }
+        }
+        for (let i=0; i<8; i+=1){
+            for (let j=0; j<8; j+=1){
+                boardPiece = curBoard[i][j]
+                if (boardPiece != "."){
+                    if (boardPiece === boardPiece.toUpperCase()){
+                        if (validateMovement([i, j], king_position, kishCheck=true)){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    else {
+        for (let i=0; i<8; i+=1){
+            for (let j=0; j<8; j+=1){
+                if (curBoard[i][j] == 'K'){
+                    king_position = [i, j];
+                }
+            }
+        }
+        for (let i=0; i<8; i+=1){
+            for (let j=0; j<8; j+=1){
+                boardPiece = curBoard[i][j]
+                if (boardPiece != "."){
+                    if (boardPiece === boardPiece.toLowerCase()){
+                        if (validateMovement([i, j], king_position, kishCheck=true)){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
 
 startGame();
 setPieceHoldEvents();
